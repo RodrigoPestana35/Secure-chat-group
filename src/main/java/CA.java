efa5c831eff400efe729020dcafd22a6bda55db0
RodrigoPestana35 <2079821@student.uma.pt>
2024-04-27 11:50:15 +0100

    feat(CA): creates the CA class constructor

--- src/main/java/CA.java
+++ src/main/java/CA.java
@@ -3,5 +3,12 @@ import java.security.KeyPairGenerator;
 import java.security.PrivateKey;
 
 public class CA {
+    private PrivateKey privateKey;
+
+    public CA() throws Exception {
+        KeyPair keyPair =  KeyPairGenerator.getInstance("RSA").generateKeyPair();
+        this.privateKey = keyPair.getPrivate();
+    }
+
 
 }
