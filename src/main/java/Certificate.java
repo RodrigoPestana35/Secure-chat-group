ef69a0108fe2296b78f74e46eae0bd1b688f84e0
RodrigoPestana35 <2079821@student.uma.pt>
2024-04-28 21:21:07 +0100

    feat(CA): creates the Certificate class constructor

--- src/main/java/Certificate.java
+++ src/main/java/Certificate.java
@@ -1,2 +1,15 @@
+import java.security.PublicKey;
+
 public class Certificate {
+    private String username;
+    private PublicKey publicRSAKey;
+    private static int ID = 1;
+    private String certificateID;
+
+    public Certificate(String username, PublicKey publicRSAKey) {
+        this.username = username;
+        this.publicRSAKey = publicRSAKey;
+        this.certificateID = "CERTIFICATE" + ID;
+        ID++;
+    }
 }
