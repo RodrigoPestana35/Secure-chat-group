e85e9b074521e6786fdb9544ec9e1b63973dee75
RodrigoPestana35 <2079821@student.uma.pt>
2024-04-28 21:52:17 +0100

    feat(CA): creates signature getters and setters

--- src/main/java/Certificate.java
+++ src/main/java/Certificate.java
@@ -5,11 +5,21 @@ public class Certificate {
     private PublicKey publicRSAKey;
     private static int ID = 1;
     private String certificateID;
+    private byte[] signature;
 
     public Certificate(String username, PublicKey publicRSAKey) {
         this.username = username;
         this.publicRSAKey = publicRSAKey;
         this.certificateID = "CERTIFICATE" + ID;
         ID++;
+    }
+
+
+    public byte[] getSignature() {
+        return signature;
+    }
+
+    public void setSignature(byte[] signature) {
+        this.signature = signature;
     }
 }
