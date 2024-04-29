2ade427730a336b4ab07fdd085f572339a929afc
RodrigoPestana35 <2079821@student.uma.pt>
2024-04-29 17:18:44 +0100

    feat(MainSender): changes the mainSender to create several Senders

--- src/main/java/MainSender.java
+++ src/main/java/MainSender.java
@@ -4,10 +4,8 @@ public class MainSender {
 
     public static void main ( String[] args ) throws Exception {
         Sender sender = new Sender ( );
-        Scanner usrInput = new Scanner ( System.in );
-        System.out.println ( "Write the message to send" );
-        String message = usrInput.nextLine ( );
-        sender.sendMessage ( message );
+        Thread clientThread = new Thread ( sender );
+        clientThread.start ( );
     }
 
 }
