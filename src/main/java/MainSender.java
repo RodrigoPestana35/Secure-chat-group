903fafc1418427bbe929b57083e1c0efc637dd6c
RodrigoPestana35 <2079821@student.uma.pt>
2024-05-12 19:05:42 +0100

    docs(MainSender): add documentation

--- src/main/java/MainSender.java
+++ src/main/java/MainSender.java
@@ -1,7 +1,15 @@
 import java.util.Scanner;
 
+/**
+ * Class that starts the client
+ */
 public class MainSender {
 
+    /**
+     * Main method
+     * @param args
+     * @throws Exception
+     */
     public static void main ( String[] args ) throws Exception {
         Sender sender = new Sender ( );
         Thread clientThread = new Thread ( sender );
