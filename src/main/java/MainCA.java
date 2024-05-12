8a4719db715c0dd7bb7136f20f663c7b41d0a985
RodrigoPestana35 <2079821@student.uma.pt>
2024-05-12 18:38:39 +0100

    feat(): implement github actions

--- src/main/java/MainCA.java
+++
@@ -1,7 +0,0 @@
-public class MainCA {
-    public static void main ( String[] args ) throws Exception {
-        CA ca = new CA ( );
-        Thread caThread = new Thread ( ca );
-        caThread.start ( );
-    }
-}
